2ede4de40b552d1d994115a19831aa01caf85f3d
MH Herock <m.h.herock@gmail.com>
2017-05-12 20:18:27 +0600

    RRF Awesome Creation !!!

---
+++ js/active-magnific-popup.js
@@ -0,0 +1,24 @@
+/* ===============================================================================
+ * Name: JS Style V3.0 || 2017 !!!
+ * Company: Banglasoft@Corporation™ !!!
+ * Founder: Engr. Md Mahadi Hassan Herock !!!
+ * Website URL-I: http://www.banglasoft.net !!!
+ * Website URL-II: http://www.bangla-soft.com !!!
+ * Description: Try To Do Something Better Than Other !!!
+ ============================================================================== */
+
+/* ====================== JS Plugins Start From Here ========================= */
+
+/* =================== Animation Plugin Start From Here ===================== */
+
+$(document).ready(function () {
+	
+	$('.video-play-btn').magnificPopup({
+		type: 'video'
+	});
+	
+});
+
+/* ====================== Animation Plugin End Here ========================= */
+
+/* ========================= JS Plugins End Here ============================= */
